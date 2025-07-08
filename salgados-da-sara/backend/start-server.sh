e4de4747745599877b278e7829eb9a5e6d113a46
Joao Pedro <hrllacvt@gmail.com>
2025-07-08 00:05:55 -0300

    atualizado funcional bonito

--- salgados-da-sara/backend/start-server.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "Iniciando servidor PHP na porta 8000..."
-echo ""
-echo "Acesse: http://localhost:8000"
-echo "Para parar o servidor, pressione Ctrl+C"
-echo ""
-php -S localhost:8000 server.php
